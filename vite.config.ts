8d5c6c7fd349859d2117fff1cb92980437227fc5
tucha <tuychin@gmail.com>
2024-05-27 01:40:40 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-const PROD_URL = 'https://tuychin.github.io/currency-rate/';
-
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? PROD_URL : '/',
+  base: command === 'build' ? '/currency-rate/' : '/',
   plugins: [react(), tsconfigPaths()],
 }));
